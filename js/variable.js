9906f131c75606919508539caecaa2949c4ad377
josephbill <josephbill00@gmail.com>
2024-06-19 12:44:29 +0300

    variable js and readme on js

---
+++ js/variable.js
@@ -0,0 +1,24 @@
+// variables are storage containers for information referencing
+// we define using two keywords , let and const : 
+// variables once defined cannot be redefined
+
+// variables defined using let are mutable :  the value referenced can be changed
+//  when coming up with the name for a variable make it descriptive as
+// possible 
+// an equal sign is used to 'assign' a value to a variable name. 
+let person_name = "Joseph Mbugua"
+// to check on changes print information to the console : console.log
+console.log(person_name)
+person_name = "mary"
+console.log(person_name)
+let second_person_name = "Alvin"
+
+
+// const is immutable : cannot change 
+const position = 1
+console.log(position)
+
+let third_person = null;
+console.log(third_person)
+
+let isfinanced = true;
